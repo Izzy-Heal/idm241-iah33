f40eafc5324c8b6aa58868ccb6957c949f77847f
Izzy Heal <iah33@drexel.edu>
2021-11-03 20:34:48 -0400

    beta build

--- beta/build/javascript.js
+++ beta/build/javascript.js
@@ -16,3 +16,20 @@
             y.className = "color";
         }
         }
+        function divdown2() {
+            var x = document.getElementById("txtbx2"); 
+            console.log("works")
+            if (x.className === "init") {
+              x.className = "slide-out-bottom";
+            }
+            else if ( x.className === "slide-out-bottom") {
+              x.className = 'slide-up';
+            }
+            else if ( x.className === "slide-up") {
+                x.className = 'slide-out-bottom';
+              }
+            var y = document.getElementById("image2");
+            if(y.className === "start"){
+                y.className = "color";
+            }
+            }
